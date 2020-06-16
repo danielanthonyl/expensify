a9f348ed1fcbd0e12c7c383656932353ada5714f
notnishi <notogloko@gmail.com>
2020-06-16 20:01:30 -0300

    setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,7 @@ import * as firebase from 'firebase';
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL:process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -14,6 +14,5 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-admin.initializeApp();
-
+firebase.analytics();
 export { firebase, database as default };
