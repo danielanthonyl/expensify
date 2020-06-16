078f09b8a5d25358911e8509760b2a6594ddebb8
notnishi <notogloko@gmail.com>
2020-06-16 19:48:14 -0300

    setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,6 +14,6 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-database.ref().set('my beautiful balzz');
+admin.initializeApp();
 
 export { firebase, database as default };
